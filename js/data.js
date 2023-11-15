93c626a8f04e27c34736043bdf93b8dbe177a919
John-bess4 <jbesss247@gmail.com>
2023-11-15 23:54:06 +0000

    issue-1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,14 @@ let data = {
   editing: null,
   nextEntryId: 1,
 };
+
+window.addEventListener('beforeunload', function(){
+  localStorage.setItem('data', JSON.stringify(data));
+});
+
+const storedData = localStorage.getItem('data');
+
+if(storedData){
+  const initialData = JSON.parse(storedData);
+  Object.assign(data, initialData);
+};
